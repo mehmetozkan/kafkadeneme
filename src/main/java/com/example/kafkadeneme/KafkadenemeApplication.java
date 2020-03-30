6e0fadcbe5c77cf210696a8da93d85448b6c1a64
Mehmet <mehmett.ozkann@gmail.com>
2020-03-31 01:38:33 +0300

    updated 31032020

--- src/main/java/com/example/kafkadeneme/KafkadenemeApplication.java
+++ src/main/java/com/example/kafkadeneme/KafkadenemeApplication.java
@@ -6,11 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkadenemeApplication {
 
-    //docker-compose -f docker-compose.yml up -d
+    //docker-compose -f docker-compose1.yml up -d
+    //docker-kafka -f docker-kafka.yml up -d
     //docker-compose -f docker-kafka.yml up -d
     //https://www.youtube.com/watch?v=dLDDCYAGNpM
     //https://github.com/HaydiKodlayalim/spring-examples
     // postman http://localhost:8080/message {"message": "Test Kuyruk1"}
+
+    //docker-compose -f <docker-compose_file_name> up -d
     public static void main(String[] args) {
         SpringApplication.run(KafkadenemeApplication.class, args);
     }
